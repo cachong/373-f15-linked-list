20819866f81feb87e92847d1b599bd0497248f7d
Caleigh Chong <cachong@localhost.localdomain>
2015-09-17 20:09:15 -0400

    finished homework2

--- list.c
+++ list.c
@@ -11,6 +11,36 @@ list_t* insert_sorted(list_t* head, list_t* new_element) {
 	assert(head != NULL);
 	assert(new_element != NULL);
 
+	list_t* temp_element;
+
+	temp_element = new_element;
+
+	//if new element belongs at end of list
+	if (head->next == NULL)
+	{
+		new_element = new_element->next;
+		head->next = temp_element;
+	}
+	//if new element belongs at beginning of list
+	else if (head->index > temp_element->index)
+	{
+		new_element = new_element->next;
+		temp_element->next = head;
+		head = temp_element;
+	}
+	//finding place for new element
+	else if (head->next->index > temp_element->index)
+	{
+		new_element = new_element->next;
+		temp_element->next = head->next;
+		head->next = temp_element;
+	}
+
+	if (new_element != NULL)
+	{
+		insert_sorted(head->next, new_element);
+	}
+
 	return head;
 }
 
@@ -18,6 +48,26 @@ list_t* insert_sorted(list_t* head, list_t* new_element) {
 // the resulting list. You do not need to preserve the original list.
 list_t* reverse(list_t* head) {
 	assert(head != NULL);
+
+	list_t* copy_element;
+
+	list_t* next_next_element;
+
+	copy_element = head->next;
+
+	head->next = NULL;
+
+	next_next_element = copy_element->next;
+
+	while(next_next_element != NULL)
+	{
+		next_next_element = copy_element->next;
+
+		copy_element->next = head;
+		head = copy_element;
+		copy_element = next_next_element;
+	}
+
 
 	return head;
 }
